7646338d971f3c285c709f911a8f8bec9ca2f803
Antoine DAUTRY <antoine.dautry@sii.fr>
2020-09-04 14:10:43 +0200

    Ajout des images

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,9 +1,34 @@
-'use strict';
+"use strict";
 
-chrome.runtime.onInstalled.addListener(details => {
-  console.log('previousVersion', details.previousVersion);
+
+
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("onInstalled...");
+  // create alarm after extension is installed / upgraded
+  chrome.alarms.create("refresh", { periodInMinutes: 1 });
+
+  chrome.storage.sync.set({ color: "#3aa757" }, function () {
+    console.log("The color is green.");
+  });
 });
 
-chrome.browserAction.setBadgeText({text: '\'Allo'});
+chrome.alarms.onAlarm.addListener((alarm) => {
+  console.log(alarm.name); // refresh
+  if (alarm.name === "refresh") {
+    helloWorld();
+  }
+});
 
-console.log('\'Allo \'Allo! Event Page for Browser Action');
+function helloWorld() {
+  let now = new Date();
+  console.log(now.toString());
+  chrome.runtime.sendMessage({ msg: "hello there" });
+  chrome.browserAction.setIcon({
+    path: {
+      19: "images/online-19.png",
+      38: "images/online-38.png",
+    },
+  });
+}
+
+// chrome.browserAction.setBadgeText({text: "BG"});
