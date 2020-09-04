b0b41a8cbaab672bcdab94c5b147d9101e7c54eb
Antoine DAUTRY <antoine.dautry@sii.fr>
2020-09-04 17:30:16 +0200

    - Ajout d'une page options TEST - Parsing et récupération des données de WAZZUP. Et des positions ouvertes - Notification de test OK

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,15 +1,11 @@
 "use strict";
 
-
-
 chrome.runtime.onInstalled.addListener(() => {
   console.log("onInstalled...");
+  chrome.browserAction.setTitle({ title: "My New Title" });
   // create alarm after extension is installed / upgraded
   chrome.alarms.create("refresh", { periodInMinutes: 1 });
-
-  chrome.storage.sync.set({ color: "#3aa757" }, function () {
-    console.log("The color is green.");
-  });
+  getStatusOfAtc();
 });
 
 chrome.alarms.onAlarm.addListener((alarm) => {
@@ -18,6 +14,39 @@ chrome.alarms.onAlarm.addListener((alarm) => {
     helloWorld();
   }
 });
+
+function getStatusOfAtc() {
+  const positionList = ["LFRS_APP", "LFBD_TWR", "DTTA_TWR", "LFBB_CTR"];
+  const REGEX_NO_OBS = /^((?!OBS).)*$/;
+  const WAZZUP_URL = "http://api.ivao.aero/getdata/whazzup/whazzup.txt";
+  Papa.parse(WAZZUP_URL, {
+    download: true,
+    delimiter: ":",
+    complete: function (results) {
+      const openAtc = [];
+      for (let i = 8; i < results.data.length; i++) {
+        const obj = results.data[i];
+        const callsign = obj[0];
+        if (obj[3] === "ATC" && callsign.match(REGEX_NO_OBS)) {
+          openAtc.push(callsign);
+        }
+      }
+
+      let result = {};
+      for (let j = 0; j < positionList.length; j++) {
+        const element = positionList[j];
+        if (openAtc.includes(element)) {
+          result[element] = true;
+        } else {
+          result[element] = false;
+        }
+      }
+      console.log(result);
+    },
+  });
+}
+
+function name(params) {}
 
 function helloWorld() {
   let now = new Date();
@@ -29,6 +58,21 @@ function helloWorld() {
       38: "images/online-38.png",
     },
   });
+  showNotification();
+}
+
+function showNotification() {
+  var openTodos = 3;
+
+  if (openTodos > 0) {
+    // Now create the notification
+    // chrome.notifications.create('reminder', {
+    //     type: 'basic',
+    //     iconUrl: 'images/online-128.png',
+    //     title: 'Don\'t forget!',
+    //     message: 'You have '+openTodos+' things to do. Wake up, dude!'
+    //  }, function(notificationId) {});
+  }
 }
 
 // chrome.browserAction.setBadgeText({text: "BG"});
--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,3 +1,11 @@
 'use strict';
 
 console.log('\'Allo \'Allo! Popup');
+
+document.querySelector('#go-to-options').addEventListener("click", function() {
+    if (chrome.runtime.openOptionsPage) {
+      chrome.runtime.openOptionsPage();
+    } else {
+      window.open(chrome.runtime.getURL('options.html'));
+    }
+  });
