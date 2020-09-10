71dd6100ca61f441a465c75f713bcf7b1e9534c6
Antoine DAUTRY <antoine.dautry@sii.fr>
2020-09-10 11:36:43 +0200

    - Ajout de la vérification dans les options

--- app/scripts.babel/options.js
+++ app/scripts.babel/options.js
@@ -2,21 +2,34 @@
 
 // Saves options to chrome.storage
 function saveOptions() {
-  const atcList = document.getElementById('atc-list').value;
-  const notifications = document.getElementById('notifications').checked;
-  chrome.storage.sync.set({
-      atcList: atcList,
-      notifications: notifications
-    }, function () {
-      // Update status to let user know options were saved.
-      let status = document.getElementById('status');
-      status.textContent = chrome.i18n.getMessage('parametersSaved');
-      status.classList.remove('d-none');
-      setTimeout(function () {
-        status.classList.add('d-none');
-      }, 1000);
+  let atcListEl = document.getElementById('atc-list');
+  let feedback = document.getElementById('atc-list-feedback-error');
+  if (atcListValidation()) {
+    const atcList = atcListEl.value;
+    const notifications = document.getElementById('notifications').checked;
+    if (atcListEl.classList.contains('is-invalid')) {
+      atcListEl.classList.remove('is-invalid');
     }
-  );
+    if (!feedback.classList.contains('d-none')) {
+      feedback.classList.add('d-none');
+    }
+    chrome.storage.sync.set({
+        atcList: atcList,
+        notifications: notifications
+      }, function () {
+        // Update status to let user know options were saved.
+        let status = document.getElementById('status');
+        status.textContent = chrome.i18n.getMessage('parametersSaved');
+        status.classList.remove('d-none');
+        setTimeout(function () {
+          status.classList.add('d-none');
+        }, 1000);
+      }
+    );
+  } else {
+    atcListEl.classList.add('is-invalid');
+    feedback.classList.remove('d-none');
+  }
 }
 
 // Restores select box and checkbox state using the preferences
@@ -37,7 +50,18 @@ function initializeTranslations() {
   document.getElementById('title').innerText = chrome.i18n.getMessage('optionsTitle');
   document.getElementById('atc-list-label').innerText = chrome.i18n.getMessage('optionsAtcListLabel');
   document.getElementById('atc-list-help').innerText = chrome.i18n.getMessage('optionsAtcListHelp');
+  document.getElementById('atc-list-feedback-error').innerText = chrome.i18n.getMessage('optionsAtcListFeedbackError');
   document.getElementById('save').innerText = chrome.i18n.getMessage('save');
+}
+
+function atcListValidation() {
+  const REGEX_ATC_LIST = /^([A-Z]{4}((_*[A-Z]{0,3}))_[A-Z]{3},*)+$/;
+  let value = document.getElementById('atc-list').value;
+  if(value.match(REGEX_ATC_LIST)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 document.addEventListener('DOMContentLoaded', initializeTranslations);
